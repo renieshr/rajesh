00226e3e85ca82bf0d3f4d510e31e7f62904748f
rr <renpost00@gmail.com>
2025-07-05 14:39:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -32,23 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-    document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    // Toggle Nav
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
 
 
     // Optional: Smooth scroll for internal links (if not using scroll-behavior: smooth in CSS)
